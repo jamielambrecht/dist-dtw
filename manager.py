a47f8599362844026d4893db7301eb1bab14c824
Jamie Lambrecht <mjlambrecht@yahoo.com>
2021-12-12 14:35:47 -0800

    Almost working. No sigterm mechanism.

--- manager.py
+++ manager.py
@@ -10,8 +10,6 @@ def matrix(value, M, N):
 comm = MPI.COMM_SELF.Spawn(sys.executable, args=['agent.py'], maxprocs=2)
 rank = comm.Get_rank()
 size = comm.Get_size()
-print("Remote group size:" + str(comm.remote_group.size))
-print("This program requires exactly 3 processes, one manager and two agents. " + "I am the manager.")
 
 A = [(2,2), (0,4), (2,6), (4,5)]
 B = [(1,1), (0,6), (4,4)]
@@ -22,7 +20,7 @@ N = len(B)
 m_n = np.array((M,N))
 print("About to send matrix dimensions (M x N): " + str(m_n))
 comm.Bcast(m_n, root=MPI.ROOT)
-    
+
 # Manager Process initializes matrices.
 cost_matrix = matrix(0, M, N) # Initialize with zeros
 
@@ -30,94 +28,108 @@ for i in range(M):
     for j in range(N):
         cost_matrix[i,j] = (A[i][0] - B[j][0])**2 + (A[i][1] - B[j][1])**2 # distance function
 
-comm.Bcast(cost_matrix, root=MPI.ROOT)
+print(cost_matrix)
 
 dtw_matrix = matrix(0, M, N) # Initialize with zeros
 dtw_matrix[0, 0] = cost_matrix[0, 0] # Initialize top left cell
 
-current_row_index, current_column_index = np.array(0), np.array(0)
-current_row = np.array(dtw_matrix[0, 1:])
-current_column = np.array(dtw_matrix[1:, 0])
+neighbor = dtw_matrix[0, 0]
+# COLUMN
+for i in range(1, M):
+    dtw_matrix[i, 0] = neighbor + cost_matrix[i, 0]
+    neighbor = dtw_matrix[i, 0]
+    print("M @ " + str(i) + ": " + str(dtw_matrix[i, 0]))
+# ROW
+neighbor = dtw_matrix[0, 0]
+for i in range(1, N):
+    dtw_matrix[0, i] = neighbor + cost_matrix[0, i]
+    neighbor = dtw_matrix[0, i]
+    print("N @ " + str(i) + ": " + str(dtw_matrix[i, 0]))
 
-update_A, update_B = True, True
+print(dtw_matrix)
+
+send_row, send_column = True, True
+req_for_row, req_for_col = [], []
+
+row_counter, col_counter = 1, 1
+row_limit, col_limit = N - 1, M - 1
+
 complete = False
-req_from_A, req_from_B = [], []
+first_time = True
 
-first_run = True
-
-
-while not complete:
-
-    if update_A:
+while row_counter < row_limit or col_counter < col_limit:
+    
+    if send_row:
         # Stuff that we need to send to the row process
-        if not first_run:
-            left_neighbor = dtw_matrix[current_row_index, current_column_index - 1]
+        previous_row = np.array(dtw_matrix[row_counter - 1, col_counter - 1:], dtype=np.int64)
+        cost_row = np.array(cost_matrix[row_counter, col_counter:], dtype=np.int64)
+        left_neighbor = np.array(dtw_matrix[row_counter, col_counter - 1], dtype=np.int64)
+
+        req_for_row.append(comm.Isend(np.array(len(previous_row), dtype=np.int64), dest=0, tag=1))
+        req_for_row.append(comm.Isend(previous_row, dest=0, tag=2))
+        req_for_row.append(comm.Isend(left_neighbor, dest=0, tag=3))
+
+        new_row = np.zeros(len(previous_row) - 1, dtype = np.int64)
+        req_for_row.append(comm.Isend(cost_row, dest=0, tag=4))
+
+    if send_column:
+        previous_col = np.array(dtw_matrix[row_counter - 1:, col_counter - 1], dtype=np.int64)
+        cost_col = np.array(cost_matrix[row_counter:, col_counter], dtype=np.int64)
+        up_neighbor = np.array(dtw_matrix[row_counter - 1, col_counter], dtype=np.int64)
+
+        req_for_col.append(comm.Isend(np.array(len(previous_col), dtype=np.int64), dest=1, tag=1))
+        req_for_col.append(comm.Isend(previous_col, dest=1, tag=2))
+        req_for_col.append(comm.Isend(up_neighbor, dest=1, tag=3))
+
+        new_col = np.zeros(len(previous_col) - 1, dtype = np.int64)
+        req_for_col.append(comm.Isend(cost_col, dest=1, tag=4))
+
+    if len(req_for_row) > 0:
+        for req in req_for_row:
+            req.wait()
+            req_for_row.clear()
+            req_for_row.append(comm.Irecv(new_row, source = 0, tag=5))
+    if len(req_for_col) > 0:
+        for req in req_for_col:
+            req.wait()
+            req_for_col.clear()
+            req_for_col.append(comm.Irecv(new_col, source = 1, tag=5))
+
+    if len(req_for_row) > 0:
+        for req in req_for_row:
+            req.wait()
+            req_for_row.clear()
+        for i in range(len(new_row)):
+            dtw_matrix[row_counter, col_counter + i] = new_row[i]
+
+    if len(req_for_col) > 0:
+        for req in req_for_col:
+            req.wait()
+            req_for_col.clear()
+        for i in range(len(new_col)):
+            dtw_matrix[row_counter + i, col_counter] = new_col[i]
+
+
+    if row_counter < row_limit:
+        row_counter += 1
+        if row_counter < row_limit:
+            send_row = True
         else:
-            left_neighbor = dtw_matrix[0, 0]
-        req_from_A.append(comm.Isend(current_row_index, dest=0, tag=0))
-        req_from_A.append(comm.Isend(current_column_index, dest=0, tag=1))
-        req_from_A.append(comm.Isend(left_neighbor, dest=0, tag=2))
-        req_from_A.append(comm.Isend(current_row, dest=0, tag=3))
-        if not first_run:
-            current_row = np.zeros(N-current_column_index, dtype=np.int64)
+            comm.Send(np.array(1, dtype=np.int64), dest=0, tag=6)
+            print("Sent sigterm to 0")
+
+    if col_counter < col_limit:
+        col_counter += 1
+        if col_counter < col_limit:
+            send_column = True
         else:
-            current_row = np.zeros(N-current_column_index, dtype=np.int64)
-        
-        print("Length of returned row: " + str(len(current_row)))
-        
-        update_A = False
-    if update_B:
-        # Stuff that we need to send to the column process
-        if not first_run:
-            up_neighbor = dtw_matrix[current_row_index - 1, current_column_index]
-        else:
-            up_neighbor = dtw_matrix[0, 0]
-        req_from_B.append(comm.Isend(current_row_index, dest=1, tag=4))
-        req_from_B.append(comm.Isend(current_column_index, dest=1, tag=5))
-        req_from_B.append(comm.Isend(up_neighbor, dest=1, tag=6))
-        req_from_B.append(comm.Isend(current_column, dest=1, tag=7))
-        if not first_run:
-            current_column = np.zeros(M-current_row_index, dtype=np.int64)
-        else:
-            current_column = np.zeros(M-current_row_index, dtype=np.int64)
-        
-        print("Length of returned column: " + str(len(current_column)))
-        
-        update_B = False
+            comm.Send(np.array(1, dtype=np.int64), dest=1, tag=6)
+            print("Sent sigterm to 1")
 
-    for req in req_from_A + req_from_B:
-        req.wait()
-
-    current_row_index += 1
-    current_column_index += 1
-
-    comm.Recv(current_row, source = 0, tag=8)
-    comm.Recv(current_column, source = 1, tag=9)
-
-    print("Manager received: \n" + str(current_row) + "\n" + str(current_column))
-    # print("Length of current row: " + str(len(current_row)))
-    if len(current_row) != 0:
-        for i in range(len(current_row)):
-            dtw_matrix[current_row_index - 1, current_column_index + i] = current_row[i]
-        
-        update_A = True
-    if len(current_row) != 0:
-        for i in range(len(current_column)):
-            dtw_matrix[current_row_index + i, current_column_index - 1] = current_column[i]
-
-        update_B = True
-
-    current_column = np.array(dtw_matrix[current_row_index:, current_column_index - 1])
-    current_row = np.array(dtw_matrix[current_row_index - 1, current_column_index:])
-
-    print("DTW Matrix:")
     print(dtw_matrix)
 
-    print("Current row " + str(current_row))
-    print("Current column " + str(current_column))
+    print("Computed:: Rows: {}, Cols: {}".format(row_counter, col_counter))
 
-    first_run = False
-
-
+print("RESULT:\n" + str(dtw_matrix))
 
 comm.Disconnect()
